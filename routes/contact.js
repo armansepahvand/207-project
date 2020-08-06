de4891325e913393e64d90b54034a93d7085e4cf
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2020-08-06 15:05:38 -0600

    Fixed Update data function

--- routes/contact.js
+++ routes/contact.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var data = require('../models/mongoose_data');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('contact', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  // Getting the agency information
+  data.getContactData(null, (error, agencies) => {
+    if (error) return res.status(500).send('Error ' + error);
+    // Get agents information 
+    data.getContactData(1, (error, agents) => {
+      res.render('contact',
+        { agencies: agencies, agents: agents });
+    })
+  })
 });
 
 module.exports = router;
--- routes/logedinuser.js
+++ routes/logedinuser.js
@@ -18,13 +18,16 @@ router.get('/', function(req, res, next) {
 // });
 
 router.post('/', function (req, res, next) {
+  console.log(req.user);
+  req.body._id=req.user._id;
   data.updateUser(req.body, (err, message) => {
-    if (err) return res.status(500).send('Error ' + err);
-    res.send("<h2 style='margin:50px;text-align: center;'>" +
+     if (err) return res.status(500).send('Error ' + err);
+     res.send("<h2 style='margin:50px;text-align: center;'>" +
       "Update was Successful</h2>" +
-      "<script> setTimeout(()=>window.location='/', 2000)</script>");
+     "<script> setTimeout(()=>window.location='/', 2000)</script>");
+    //res.render('logedinuser', { title: 'Express' });
   });
-  res.render('logedinuser', { title: 'Express' });
+  
 });
 
 module.exports = router;
