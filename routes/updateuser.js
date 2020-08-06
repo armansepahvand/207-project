8e62feae2665a9d9a55c234a54f4fc7188e082bf
Duncan MacMinn <duncanmacminn@gmail.com>
2020-08-06 11:16:44 -0600

    minor cleanup of updateuser.js, and corrections to id names in logedinuser and register

--- routes/updateuser.js
+++ routes/updateuser.js
@@ -7,18 +7,8 @@ router.get('/', function(req, res, next) {
   res.render('logedinuser', { title: 'Express' });
 });
 
-router.post('/', (req, res, next) => {
-  //console.log(JSON.stringify(req.body));
-  data.createUser(req.body, (err, message) => {
-    if (err) return res.status(500).send('Error ' + err);
-    res.send("<h2 style='margin:50px;text-align: center;'>" +
-      "Thank You for registering</h2>" +
-      "<script> setTimeout(()=>window.location='/', 2000)</script>");
-  });
-});
-
 //Basel's work on User Update function
-router.put('/logedinuser', currentUser, function(req,res,next){
+router.post('/logedinuser', currentUser, function(req,res,next){
   Customers.findById(req.customer._id).then(function(customer){
       if(!currentUser){return res.sendStatus(401);}
       if(typeof req.body.customer.CustFirstName !== 'undefined'){
