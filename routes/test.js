3ae1d1af47c454940a212faad3cab66b5e0878ea
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2020-08-07 08:22:02 -0600

    fixed a typo

--- routes/test.js
+++ routes/test.js
@@ -13,7 +13,7 @@ router.post('/', (req, res, next) => {
   data.createBooking(req.body, (err, message) => {
     if (err) return res.status(500).send('Error ' + err);
     res.send("<h2 style='margin:50px;text-align: center;'>" +
-      "Thank You for bokking this package</h2>" +
+      "Thank You for booking this package</h2>" +
       "<script> setTimeout(()=>window.location='/', 2000)</script>");
   });
 });
