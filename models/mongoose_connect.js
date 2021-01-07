c4cc0809f5b50bf9354cb5672ebb915a06cdf660
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-01-06 21:16:25 -0700

    frixed some issues with the comments at the top of the pug view files

--- models/mongoose_connect.js
+++ models/mongoose_connect.js
@@ -1,13 +1,16 @@
 // Created by Crystal, edited by other members
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 //const url = "mongodb://localhost:27017/travelexperts";
-const url = "mongodb+srv://ThunderstormPantherBearAdmin:ThunderstormPantherBear@thunderstormpantherbear.wmwvd.azure.mongodb.net/travelexperts?retryWrites=true&w=majority"
+const url =
+  "mongodb+srv://ThunderstormPantherBearAdmin:ThunderstormPantherBear@thunderstormpantherbear.wmwvd.azure.mongodb.net/travelexperts?retryWrites=true&w=majority";
 
 // Set up a mongoose connection
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 // Get the connection
 const db = mongoose.connection;
 // Bind connection to error event (to get notification of connection errors)
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-module.exports = mongoose
+mongoose.set("useCreateIndex", true);
+
+module.exports = mongoose;
